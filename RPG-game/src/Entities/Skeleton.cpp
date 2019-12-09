432d80ec41d67e57efa5314f45d33be2486cedac
Vitaljaz <43145747+Vitaljaz@users.noreply.github.com>
2019-12-09 21:05:51 +0300

    update Skeleton class

--- RPG-game/src/Entities/Skeleton.cpp
+++ RPG-game/src/Entities/Skeleton.cpp
@@ -1,17 +1,11 @@
 #include "..\stdafx.h"
 #include "Skeleton.h"
 
-Skeleton::Skeleton(float x, float y, sf::Texture & textureSheet)
+Skeleton::Skeleton(float x, float y, sf::Texture& textureSheet, Player* target)
 {
+	initVariables(target);
+	initComponets(textureSheet);
 	setPosition(x, y);
-
-	createHitboxController(sprite, 18.f, 18.f, 50.f, 60.f);
-	createMovementController(300.f, 15.f, 5.f);
-	createAnimationController(textureSheet);
-
-	animationController->addAnimation("IDLE", 70.f, 0, 0, 3, 0, 128, 96);
-	animationController->addAnimation("WALK", 20.f, 0, 1, 5, 1, 128, 96);
-	animationController->addAnimation("ATTACK", 20.f, 0, 2, 7, 2, 128, 96);
 }
 
 Skeleton::~Skeleton()
@@ -28,4 +22,25 @@ void Skeleton::update(const float dt)
 void Skeleton::updateAnimations(const float dt)
 {
 	animationController->play("IDLE", dt, movementController->getVelocity().x, movementController->getMaxVelocity());
+}
+
+void Skeleton::initComponets(sf::Texture & textureSheet)
+{
+	createHitboxController(sprite, 18.f, 36.f, 50.f, 60.f);
+	createMovementController(300.f, 15.f, 5.f);
+	createAnimationController(textureSheet);
+
+	animationController->addAnimation("IDLE", 70.f, 0, 0, 3, 0, 128, 96);
+	animationController->addAnimation("WALK", 20.f, 0, 1, 5, 1, 128, 96);
+	animationController->addAnimation("ATTACK", 20.f, 0, 2, 7, 2, 128, 96);
+}
+
+void Skeleton::initVariables(Player * target)
+{
+	this->target = target;
+	attacking = false;
+	health = 100;
+	visibleRange = 300;
+	attackRange = 60;
+	damage = 20;
 }
--- RPG-game/src/Entities/Skeleton.h
+++ RPG-game/src/Entities/Skeleton.h
@@ -1,16 +1,20 @@
 #pragma once
-#include "Entity.h"
+#include "Enemy.h"
+#include "Player.h"
 
-class Skeleton : public Entity
+class Skeleton : public Enemy
 {
 public:
-	Skeleton(float x, float y, sf::Texture& textureSheet);
+	Skeleton(float x, float y, sf::Texture& textureSheet, Player* target);
 	virtual ~Skeleton();
 
 	virtual void update(const float dt) override;
 
 private:
 	void updateAnimations(const float dt);
+
+	void initComponets(sf::Texture& textureSheet);
+	void initVariables(Player* target);
 
 	bool attacking;
 };
